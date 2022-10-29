d3f21d454a1894c57de0ab8dd4dea96c09178d49
MosesTheSweed <116502113+MosesTheSweed@users.noreply.github.com>
2022-10-29 13:59:13 -0400

    Fixing some background styling issues (#3)

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -2,7 +2,7 @@
 const Card = ({title, children}) => {
 
   return (
-    <div className='w-full pt-6'>
+    <div className='w-full px-7 pt-6'>
       <div className='bg-sky-900 rounded-lg overflow-hidden mb-1'>
         <div className='p-4 text-left'>
           <h3 className='text-lg text-neutral-200 mb-2 block'>
