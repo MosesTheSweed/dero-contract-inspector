ae5dbe20bb41bf0f635d9e6338e33546827e361f
MosesTheSweed <MosesTheSweed@proton.me>
2022-11-17 21:23:50 -0500

    Adding a modal so I can convert contract vars and display

---
+++ src/components/common/Modal.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    width: '70%',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    backgroundColor: '#1F2937'
+  }
+};
+
+function MyModal(props) {
+  console.log('PROPS', props)
+  function afterOpenModal(e) {
+    props.onAfterOpen(e, 'After Modal Opened');
+  }
+
+  function onModalClose(event) {
+    let data = { name: 'example', type: 'closed from child' };
+    props.onCloseModal(event, data);
+  }
+
+  return (
+    <div>
+      <Modal
+        isOpen={props.IsModalOpened}
+        onAfterOpen={e => afterOpenModal(e)}
+        style={customStyles}
+        ariaHideApp={false}
+      >
+        <div className="flex flex-row justify-between bg-purple-800 text-neutral-200 px-3">
+          <h2>{props.dynData && props.dynData.name}</h2>
+          <button onClick={e => onModalClose(e)}>Close</button>
+        </div>
+        <div>
+          <ul className='text-neutral-200'>
+            {props.dynData && props.dynData.value}
+          </ul>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default MyModal;
--- src/components/common/SimpleTable.jsx
+++ src/components/common/SimpleTable.jsx
@@ -1,29 +1,61 @@
 import {VerticalScroll} from '/src/components/common/styles/VerticalScroll.jsx';
+import {useState} from 'react'
+import MyModal from '/src/components/common/Modal';
+import {ContractUtils} from '/src/utils/contractHelper.js';
 
 const SimpleTable = ({headers, data}) => {
+  const [IsModalOpen, IsModalOpenSet] = useState(false);
+  const [modalData, modalDataSet] = useState({})
+
+  const handleOpenModal = (cvar) => {
+    modalDataSet({name: cvar.name, value: ContractUtils.hexToJson(cvar.value)})
+    openModal()
+  }
+  const openModal = () => {
+    IsModalOpenSet(true)
+  }
+  const closeModal = () => {
+    IsModalOpenSet(false)
+  }
+  const handleAfterOpen = (event, data) => {
+    console.log(event, data)
+  }
 
   return (
-    <div className="md:px-16 pb-6 w-full">
-      <div className="shadow overflow-auto rounded border-b border-gray-200">
-        <VerticalScroll>
-        <table className="min-w-full bg-white">
-          <thead className="bg-purple-800 text-neutral-200">
-          <tr>
-            {headers && headers.map((item) => <th className="w-1/3 text-left py-3 px-4 font-semibold text-sm" key={item}>{item}</th>)}
-          </tr>
-          </thead>
-          <tbody className="text-gray-700">
-            {data && data.map((item, index) =>
-              <tr className={index % 2 === 0 ? 'bg-purple-100' : 'bg-neutral-100'} key={item.name}>
-                <td className="w-1/3 text-left py-3 px-4">{item.name}</td>
-                <td className="w-1/3 text-left py-3 px-4">{item.value}</td>
+    <>
+      <div className="md:px-16 pb-6 w-full">
+        <div className="shadow overflow-auto rounded border-b border-gray-200">
+          <VerticalScroll>
+            <table className="min-w-full bg-white">
+              <thead className="bg-purple-800 text-neutral-200">
+              <tr>
+                {headers && headers.map((item) => <th className="w-1/3 text-left py-3 px-4 font-semibold text-sm"
+                                                      key={item}>{item}</th>)}
               </tr>
-            )}
-          </tbody>
-        </table>
-      </VerticalScroll>
+              </thead>
+              <tbody className="text-gray-700">
+              {data && data.map((item, index) =>
+                <tr className={index % 2 === 0 ? 'bg-purple-100' : 'bg-neutral-100'} key={item.name}>
+                  <td className="w-1/3 text-left py-3 px-4 cursor-pointer hover:text-lg hover:text-purple-800"
+                    onClick={() => handleOpenModal(item)}
+                  >
+                    {item.name}
+                  </td>
+                  <td className="w-1/3 text-left py-3 px-4">{item.value}</td>
+                </tr>
+              )}
+              </tbody>
+            </table>
+          </VerticalScroll>
+        </div>
       </div>
-    </div>
+      <MyModal
+        dynData={modalData}
+        IsModalOpened={IsModalOpen}
+        onCloseModal={closeModal}
+        onAfterOpen={handleAfterOpen}
+      />
+    </>
   )
 }
 
