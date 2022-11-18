e6601abf1d9cac7df88540071027f1356a44685d
MosesTheSweed <116502113+MosesTheSweed@users.noreply.github.com>
2022-11-17 21:25:30 -0500

    Adding a modal so I can convert contract vars and display (#16)

--- src/utils/contractHelper.js
+++ src/utils/contractHelper.js
@@ -112,6 +112,16 @@ export class ContractUtils {
     return atomicUnits / factor
   }
 
+  static hexToJson = (hexData) => {
+    let hex  = hexData.toString();
+    let str = '';
+    for (let n = 0; n < hex.length; n += 2) {
+      str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+
+    return str;
+  }
+
   static getFunctionMonetaryInputs = () => {
     return [
       {
